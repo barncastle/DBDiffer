a3531205b1c54b5d5fbba6c150d2901977089180
barncastle <spearman.tom@gmail.com>
2019-05-05 14:22:15 +0100

    added DBDiffResult helper methods

--- DBDiffer/DBDiffResult.cs
+++ DBDiffer/DBDiffResult.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace DBDiffer
 {
@@ -7,5 +9,16 @@ namespace DBDiffer
         public int[] AddedRecords;
         public int[] RemovedRecords;
         public IDictionary<int, List<Diff>> ChangedRecords;
+
+        public void Save(string path, Formatting formatting = Formatting.None)
+        {
+            using (var fs = File.CreateText(path))
+                fs.Write(JsonConvert.SerializeObject(this, formatting));
+        }
+
+        public string ToJSONString(Formatting formatting = Formatting.None)
+        {
+            return JsonConvert.SerializeObject(this, formatting);
+        }
     }
 }
